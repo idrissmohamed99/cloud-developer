78cfcb5b21982dc0578c012254af044accd20396
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-10-10 18:18:23 +0100

    Stringify ```item: todo```

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -1,22 +1,22 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import 'source-map-support/register'
 import { createTodo } from '../../helpers/todos'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
- const userId= getUserId(event)
- const todo = await createTodo(userId, newTodo)
+    const userId = getUserId(event)
+    const todo = await createTodo(userId, newTodo)
 
     return {
 
-      statusCode:201,
-      body: JSON.stringify({todo})
+      statusCode: 201,
+      body: JSON.stringify({ item: todo })
     }
   }
 )
