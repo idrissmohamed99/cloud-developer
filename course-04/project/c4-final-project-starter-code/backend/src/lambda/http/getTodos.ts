14f1bde6c690e93ce547645466abc5aff7a5acb8
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-10-10 18:16:08 +0100

    Stringify ```todo.Items```

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,10 +1,10 @@
-import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+import 'source-map-support/register'
 
 import { getAllTodos } from '../../helpers/todos'
-import { getUserId } from '../utils';
+import { getUserId } from '../utils'
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
@@ -17,7 +17,7 @@ export const handler = middy(
     return {
 
       statusCode: 200,
-      body: JSON.stringify({  todos })
+      body: JSON.stringify({ items: todos.Items })
     }
   })
 handler
